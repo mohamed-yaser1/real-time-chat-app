946074c2f86e1b989b9ec997ccac0565f23285a2
mohamed <ymo64805@gmail.com>
2026-08-05 15:59:37 +0300

    initializing forntend part using vite tailwind and schadcn

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <Button variant={'secondary'} className={'text-red-500'}>click me</Button>
+    </>
+  )
+}
+
+export default App
